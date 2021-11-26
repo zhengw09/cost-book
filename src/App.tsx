eef701b3d185b8dcc0b6efcfb01c0cb45c919c73
zhngwa <zhngwa@amazon.com>
2021-11-25 23:12:42 -0800

    React table demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,112 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useTable } from "react-table";
+
+type TransactionRecord = {
+    symbol: string;
+    date: string;
+    price: number;
+    quantity: number;
+    totalQuantity: number;
+    costBasis: number;
+    totalCost: number;
+}
+
+type ColumnDefinition<T, K extends keyof T> = {
+    Header: string;
+    accessor: K;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const columns: ColumnDefinition<TransactionRecord, keyof TransactionRecord>[] = [
+        {
+            Header: "Symbol",
+            accessor: "symbol"
+        },
+        {
+            Header: "Date",
+            accessor: "date"
+        },
+        {
+            Header: "Price",
+            accessor: "price"
+        },
+        {
+            Header: "Quantity",
+            accessor: "quantity"
+        },
+        {
+            Header: "Cost Basis",
+            accessor: "costBasis"
+        },
+        {
+            Header: "Total Quantity",
+            accessor: "totalQuantity"
+        },
+        {
+            Header: "Total Cost",
+            accessor: "totalCost"
+        }
+    ]
+
+    const data: TransactionRecord[] = [
+        {
+            symbol: "TEST",
+            date: "20211124",
+            price: 10,
+            quantity: 100,
+            totalQuantity: 100,
+            costBasis: 10,
+            totalCost: 10,
+        },
+        {
+            symbol: "TEST",
+            date: "20211124",
+            price: 10,
+            quantity: 100,
+            totalQuantity: 100,
+            costBasis: 10,
+            totalCost: 10,
+        }
+    ];
+
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+      } = useTable({
+        columns,
+        data,
+      })
+
+      return (
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th {...column.getHeaderProps()}>{column.render('Header')}</th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map((row, i) => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+    );
 }
 
 export default App;
